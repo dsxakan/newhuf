842ec94d6ed3f111e046c1db00e7f7d3cde7e567
dsxakan <123813540+dsxakan@users.noreply.github.com>
2023-03-03 23:50:18 +0300

    Add files via upload

---
+++ index (1).cpp
@@ -0,0 +1,146 @@
+// C++ program to encode and decode a string using
+// Huffman Coding.
+#include <bits/stdc++.h>
+#define MAX_TREE_HT 256
+using namespace std;
+
+map<char, string> codes;
+
+map<char, int> freq;
+
+// A Huffman tree node
+struct MinHeapNode
+{
+    char data;                 // One of the input characters
+    int freq;                  // Frequency of the character
+    MinHeapNode *left, *right; // Left and right child
+
+    MinHeapNode(char data, int freq)
+    {
+        left = right = NULL;
+        this->data = data;
+        this->freq = freq;
+    }
+};
+
+struct compare
+{
+    bool operator()(MinHeapNode *l, MinHeapNode *r)
+    {
+        return (l->freq > r->freq);
+    }
+};
+
+void printCodes(struct MinHeapNode *root, string str)
+{
+    if (!root)
+        return;
+    if (root->data != '$')
+        cout << root->data << ": " << str << "\n";
+    printCodes(root->left, str + "0");
+    printCodes(root->right, str + "1");
+}
+
+void storeCodes(struct MinHeapNode *root, string str)
+{
+    if (root == NULL)
+        return;
+    if (root->data != '$')
+        codes[root->data] = str;
+    storeCodes(root->left, str + "0");
+    storeCodes(root->right, str + "1");
+}
+
+priority_queue<MinHeapNode *, vector<MinHeapNode *>, compare>
+    minHeap;
+
+void HuffmanCodes(int size)
+{
+    struct MinHeapNode *left, *right, *top;
+    for (map<char, int>::iterator v = freq.begin();
+         v != freq.end(); v++)
+        minHeap.push(new MinHeapNode(v->first, v->second));
+    while (minHeap.size() != 1)
+    {
+        left = minHeap.top();
+        minHeap.pop();
+        right = minHeap.top();
+        minHeap.pop();
+        top = new MinHeapNode('$',
+                              left->freq + right->freq);
+        top->left = left;
+        top->right = right;
+        minHeap.push(top);
+    }
+    storeCodes(minHeap.top(), "");
+}
+
+void calcFreq(string str, int n)
+{
+    for (int i = 0; i < str.size(); i++)
+        freq[str[i]]++;
+}
+
+string decode_file(struct MinHeapNode *root, string s)
+{
+    string ans = "";
+    struct MinHeapNode *curr = root;
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] == '0')
+            curr = curr->left;
+        else
+            curr = curr->right;
+
+        if (curr->left == NULL and curr->right == NULL)
+        {
+            ans += curr->data;
+            curr = root;
+        }
+    }
+    return ans + '\0';
+}
+
+// Driver code
+int main()
+{
+    string str, filename;
+    string encodedString, decodedString;
+
+    int number;
+
+    cout << "Enter text:  ";
+    getline(cin, str);
+
+    cout << "Enter filename:  ";
+    getline(cin, filename);
+
+    cout << "Encoded: 1 \n Decoded: 0" << endl;
+    cin >> number;
+
+    ofstream writeFiles(filename);
+
+    calcFreq(str, str.length());
+    HuffmanCodes(str.length());
+    cout << "Character With there Frequencies:\n";
+    for (auto v = codes.begin(); v != codes.end(); v++)
+        cout << v->first << ' ' << v->second << endl;
+
+    for (auto i : str)
+        encodedString += codes[i];
+
+    decodedString = decode_file(minHeap.top(), encodedString);
+
+    if (number == 1)
+    {
+        writeFiles << "\nEncoded Huffman data:\n"
+                   << encodedString << endl;
+    }
+    else if (number == 0)
+    {
+        writeFiles << "\nDecoded Huffman Data:\n"
+                   << decodedString << endl;
+    }
+
+    return 0;
+}
